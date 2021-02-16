563f4a08af0aa8e80937fe492ec87161d2ddb511
Jan Gevaert <jan.gevaert@gmail.com>
2021-02-16 16:07:04 -0700

    initial commit

---
+++ LinkedStack/src/edu/cnm/deepdive/Main.java
@@ -0,0 +1,23 @@
+package edu.cnm.deepdive;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+
+    Employee janeJones = new Employee("Jane", "Jones", 123);
+    Employee johnDoe = new Employee("John", "Doe", 4567);
+    Employee marySmith = new Employee("Mary", "Smith", 22);
+    Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+    Employee billEnd = new Employee("Bill", "End", 78);
+
+    //stack.printStack();
+
+    //System.out.println(stack.peek());
+    //stack.printStack();
+
+    //System.out.println("Popped: " + stack.pop());
+    //System.out.println(stack.peek);
+  }
+
+}
